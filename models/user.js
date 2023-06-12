0d83996ba6837237413db609911acc0f25aac0cb
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-12 13:18:39 +0530

    models file for inventory app

---
+++ models/user.js
@@ -0,0 +1,21 @@
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true,
+    },
+    itemname:Sequelize.STRING,
+    description:Sequelize.STRING,
+    amount:Sequelize.INTEGER,
+    quantity:Sequelize.INTEGER,
+});
+
+
+module.exports = User;
+
